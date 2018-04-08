8b7ee31a658d930cb9b32319aad6b9ae6b749891
Joe Eames <joeeames@gmail.com>
2018-04-07 23:52:05 -0600

    removed incorrect comments in strength pipe

--- src/app/strength/strength.pipe.ts
+++ src/app/strength/strength.pipe.ts
@@ -1,14 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-/*
- * Show Weak, Strong, Unbelievable
- * Takes an exponent argument that defaults to 1.
- * Usage:
- *   value | exponentialStrength
- * Example:
- *   {{ 2 |  exponentialStrength}}
- *   formats to: 1024
-*/
 @Pipe({
   name: 'strength'
 })
