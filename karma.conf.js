f776d7d4164f08f791ab9d8605bbb0c59b48ec87
Joe Eames <joeeames@gmail.com>
2021-09-06 23:20:50 -0600

    updated to v12

--- karma.conf.js
+++ karma.conf.js
@@ -9,16 +9,22 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
+    jasmineHtmlReporter: {
+      
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/PSAngularUnitTestingCourse'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
